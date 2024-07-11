ff06478762f22dc5b6f40bcd8643715bafb7cfd8
Shane Lin <shane.ext@gmail.com>
2024-07-10 23:31:11 -0400

    switch from image datetime to exif DateTimeOriginal

--- phototime.py
+++ phototime.py
@@ -4,6 +4,7 @@ import exifread
 import datetime
 import collections
 import csv
+from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('dirs', metavar='DIR', type=str, nargs='+',
@@ -11,32 +12,46 @@ parser.add_argument('dirs', metavar='DIR', type=str, nargs='+',
 
 args = parser.parse_args()
 timeslots = collections.defaultdict(set)
+count = defaultdict(list)
 
 for dir in args.dirs:
     for root, dirs, filenames in os.walk(dir):
         for filename in filenames:
             try:
-                if filename[-3:].lower() in ["dng", "tif", "arw","psd"]:
+                # print(filename)
+                if filename[-3:].lower() in ["dng", "tif", "arw","psd","jpg"]:
                     with open(os.path.join(root, filename),'rb') as file:
                         tags = exifread.process_file(file)
-                        time = datetime.datetime.strptime(str(tags['Image DateTime']), "%Y:%m:%d %H:%M:%S")
+                        # print(tags)
+                        time = datetime.datetime.strptime(str(tags['EXIF DateTimeOriginal']), "%Y:%m:%d %H:%M:%S")
+                        # print(time)
                         round_time = time.strftime("%Y-%m-%d %H:")+str((time.minute//15)*15)
                         if round_time[-2:] == ":0":
                             round_time=round_time+"0"
                         timeslots[str(time.date())].add(round_time)
+                        count[time.strftime("%Y-%m-%d")].append(filename)
             except OSError as err:
+                print(OSError)
                 pass
 
-print("\n\n Dates:")
+print("\n\nDates:")
 total = 0
 for d in sorted(timeslots.keys()):
     print(d+"\t"+str(len(timeslots[d])*.25))
+    print("   ",)
     total += len(timeslots[d])*.25
 print("\n\nTotal time: "+str(total))
+
+print("\n\nCount")
+total = 0
+for d in sorted(count):
+    print("\t"+d+": "+str(len(count[d])))
+    total += len(count[d])
+    # print("\t\t",count[d])
+print("Total:" + str(total))
 
 with open('timesheet.csv', 'w') as f:
     f.write("Date,Photography,Processing\n")
     for d in sorted(timeslots.keys()):
         f.write(d+", "+str(len(timeslots[d])*.25)+", "+str(len(timeslots[d])*.25)+"\n")
-    
     f.write("\nTotal:,"+str(total*2)+" hours")
